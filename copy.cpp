718004da5d494e149052126fc096594a357b7f18
unknown <malinimj24@gmail.com>
2025-05-23 00:56:02 -0700

    day 3

---
+++ copy.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class Student{
+public:
+int *marks;
+int size;
+Student(int s)
+{
+    size = s;
+    marks = new int[size];
+}
+Student(const Student& s)
+{
+    size =s.size;
+    marks = new int[size];
+    for(int i=0;i<size;i++)
+    {
+        marks[i] = s.marks[i];
+    }
+}
+    ~Student()
+    {
+       delete[] marks;
+
+    }
+    void display()
+    {
+        for(int i=0 ; i<size ; i++)
+        {
+            cout<<marks[i]<<"  ";
+        }
+        cout<<endl;
+    }
+};
+int main()
+{
+    Student s1(3);
+    s1.marks[0]=80  ;
+    s1.marks[1]=90  ;
+    s1.marks[2]=85  ;
+    Student s2=s1;
+    s2.display();
+    return 0;
+
+
+}
---
+++ pra.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+class Car {
+
+    string model;
+
+    int year;
+
+public:
+
+
+    Car() {
+
+        model = "Unknown";
+
+        year = 0;
+
+    }
+
+
+    Car(string m) {
+
+        model = m;
+
+        year = 0;
+
+    }
+
+
+    Car(string m, int y) {
+
+        model = m;
+
+        year = y;
+
+    }
+
+    void displayInfo() {
+
+        cout << "Model: " << model << ", Year: " << year << endl;
+
+    }
+
+};
+
+int main() {
+
+    Car car1;                   
+
+    Car car2("Toyota");         
+
+    Car car3("Honda", 2022);   
+
+    car1.displayInfo();
+
+    car2.displayInfo();
+
+    car3.displayInfo();
+
+    return 0;
+
+}
+
